a9714f19c02f5193b34fed8828ab54fb0c5e4a45
qingxuan <925342921@qq.com>
2026-05-03 01:47:00 +0800

    chore: cleanup project root by removing temporary test files and logs while centralizing backend logs

--- test_direct_start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "JAVA_CMD=C:\Users\92534\.jdks\temurin-24\bin\java.exe"
-cd /d "%~dp0java-backend"
-start /MIN "" "%JAVA_CMD%" -jar target\cycling-route-backend-1.0.0.jar
-timeout /t 15 /nobreak >nul
